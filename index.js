58d6a70d883d7f9e4f89896e004b290d34766576
amir maavia <Maaviyamansoori222@gmail.com>
2020-07-18 23:46:20 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// var cloud = document.querySelector('.cloud')
+// var animate = cloud.animate([
+//     { transform: 'translateX(100%)' },
+//     { transform: 'translateX(-100%)' }   
+// ],{
+//     duration: 20000,
+//     iterations: Infinity
+// }
+
+// )
+var grass = document.querySelector('.grass')
+var animate = grass.animate([
+    { transform: 'translateX(100%)' },
+    { transform: 'translateX(-100%)' }   
+],{
+    duration: 20000,
+    iterations: Infinity
+}
+
+)
+var tiger = document.querySelector('.insane')
+var animate = tiger.animate([
+   { transform: 'translateX(-100%)' },
+        { transform: 'translateX(100%)' }   
+    ],{
+        duration: 10000,
+        iterations: Infinity
+    }
+    
+    )
